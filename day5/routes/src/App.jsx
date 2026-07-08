4eb2e2d1a361d41021db984b1be401daedcdfea8
Prajan Gowtham <prajangowtham2004@gmail.com>
2026-07-08 12:04:07 +0530

    updated day5

---
+++ day5/routes/src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+
+
+
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <NavBar />
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ day5/routes/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar"
+
+
+
+const Home = () => {
+  return (
+    <>
+    <div ClassName="bg-red-500 ">
+        <p>Home</p>
+    </div>
+    </>
+  )
+}
+
+export default Home
